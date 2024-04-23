cfdbd478f0b2c04aaa19828dbdbc00ca0afff969
TDgraph <taymeedouglas07@gmail.com>
2024-04-23 03:10:45 -0700

    award

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const FirstLine = 'c is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);;
