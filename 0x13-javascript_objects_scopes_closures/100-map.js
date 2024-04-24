96d3faa22f78e40139dc9946759502706ea09c94
TDgraph <taymeedouglas07@gmail.com>
2024-04-23 21:21:27 -0700

    advance tasks

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script that imports an array and computes a new array.
+
+const originalList = require('./100-data').list;
+console.log(originalList);
+const mappedList = originalList.map (function (i, index) {
+    return (i * index);
+});
+console.log(mappedList);
